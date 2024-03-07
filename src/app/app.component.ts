dbf1bb0a8db1d959363e354d3beef6885014950a
BehoodiMehdi <Behboodimehdi@yahoo.com>
2024-03-07 12:04:27 +0330

    setup standalone config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports:[CommonModule,AppComponent,CourseCardComponent]
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+// @NgModule({
+//     declarations: [
+        
+//     ],
+//     providers: [],
+//     bootstrap: [],
+//     imports: [
+//         BrowserModule,
+//         BrowserAnimationsModule,
+//     ]
+// })
+
+bootstrapApplication(AppComponent)
 export class AppModule { }
